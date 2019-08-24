e4d6b711fc10606060a78b70f2b618c64676bef3
Arturo Elias Anton <arturoeanton@gmail.com>
2019-08-24 13:11:49 -0300

    init

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"encoding/xml"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
@@ -17,33 +18,36 @@ type Person struct {
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, `{"alive": true}`)
 }
 func reqJSONName(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(Person{Name: vars["name"]})
 }
 func reqHTML(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "<html><head><title>test</title></head><body><h1>Titulo</h1></body></html>")
+	t := template.Must(template.ParseFiles("template.templ"))
+	_ = t.Execute(w, Person{Name: "tim"})
 }
 func reqJSON(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(Person{Name: "tim"})
 }
 func reqXML(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "application/xml")
-	w.WriteHeader(http.StatusOK)
 	xml.NewEncoder(w).Encode(Person{Name: "tim"})
 }
 
-// only for demo
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/health", produces("json", healthCheckHandler))
+	r.HandleFunc("/", produces("html", reqHTML))
+	r.HandleFunc("/html", produces("html", reqHTML))
+	r.HandleFunc("/json", produces("json", reqJSON))
+	r.HandleFunc("/json/{name}", produces("json", reqJSONName))
+	r.HandleFunc("/xml", produces("xml", reqXML)).Methods(http.MethodGet)
+	r.Use(middleware)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":9090", nil))
+}
+
 func middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
@@ -52,15 +56,24 @@ func middleware(next http.Handler) http.Handler {
 	})
 }
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/health", healthCheckHandler)
-	r.HandleFunc("/", reqHTML)
-	r.HandleFunc("/html", reqHTML)
-	r.HandleFunc("/json", reqJSON)
-	r.HandleFunc("/json/{name}", reqJSONName)
-	r.HandleFunc("/xml", reqXML).Methods(http.MethodGet)
-	r.Use(middleware)
-	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":9090", nil))
+func produces(format string, f func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	value := format
+	if format == "json" {
+		value = "application/json"
+	}
+	if format == "xml" {
+		value = "application/xml"
+	}
+	if format == "html" {
+		value = "text/html; charset=utf-8"
+	}
+	if format == "text" {
+		value = "text/plain; charset=utf-8"
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", value)
+		w.WriteHeader(http.StatusOK)
+		f(w, r)
+	}
 }
